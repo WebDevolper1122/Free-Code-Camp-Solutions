e397a76dabeeb6bf0711f8e78ba2e68f9502d176
WebDevolper1122 <sb2137842@gmail.com>
2022-12-09 18:59:55 +0500

    FCC

---
+++ reverseAString.js
@@ -0,0 +1,25 @@
+// ==============>> Reverse-A-String <<============== //
+function reverseString(str){
+    let result = []
+   let convertArr = str.split("") 
+   let reverseArr = convertArr.reverse()
+   reverseArr.forEach(element => {
+   result.push(element)
+})
+ return result.join("");
+}
+const result = reverseString("hello")
+console.log(result);
+
+// ==============>> Second-Method <<============== //
+
+function reverseStringMethod2(str){
+    let reverseString = "";
+    for(let i=str.length -1; i>=0; i--){
+        let individualReverseElement = str[i]
+        reverseString += individualReverseElement
+    }
+    return reverseString
+}
+const value = reverseStringMethod2("hello")
+console.log(value);
