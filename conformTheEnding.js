358a8a2ea7d2c5137870cea309d4e03ce0838169
WebDevolper1122 <sb2137842@gmail.com>
2022-12-09 23:12:56 +0500

    confirmTheEnding

---
+++ conformTheEnding.js
@@ -0,0 +1,8 @@
+//------------->> Confirm The Ending <<-------------//
+function confirmEnding(str, target) {
+    if (str.substr(-target.length) === target) {
+        return true
+    } else { return false }
+}
+
+console.log(confirmEnding("Bastian", "n"));
